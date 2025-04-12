056cccd51252a554affe23abe96d182c60f0b9ea
portner-boop <damirmagdeev1054@gmail.com>
2025-04-12 15:31:34 +0400

    base commit

---
+++ src/main/java/org/example/springboot/restapifortest/RestApiForTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.springboot.restapifortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiForTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiForTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springboot/restapifortest/RestApiForTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springboot.restapifortest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiForTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
